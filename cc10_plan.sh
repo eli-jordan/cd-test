eceb7b615b5a0a10d078c6a68326fb930c26c233
Eli Jordan <elias.jordan@tapad.com>
2020-04-04 22:02:36 +0200

    cc10 config test

--- cc10_plan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export CLOUDCRE10_API_HOST="cloudcre10-stg.svc.tapad.com"
-JAR="$(ls $HOME/code/mart-framework/platform/cloudcre10-cli/target/scala-2.12/cloudcre10-cli-assembly*.jar)"
-
-java -jar "$JAR" plan config --environment-id=$(cat env-id) --config-file=environment.json
